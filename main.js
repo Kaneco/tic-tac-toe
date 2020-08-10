097272a1d36b4fc94637436fa4ca8c4dcb76d9c1
kaneco <zegomes@gmail.com>
2020-08-10 21:05:10 +0100

    Added Render function from the gameBoard to page

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ const playerFactory = (name, sign, score) => {
 
 // Board Operations and Methods
 const gameBoard = (() => { 
-        var board = new Array(9); //Creates board to store player positions
+        // var board = new Array(9); //Creates board to store player positions
+        let board = ["X", "X", "X","O", "O", "O","","",""];
 
         function changeBoard(position, sign){ //Update play made to the board, checking first if position is empty
             if (checkIfEmpty(position)) {
@@ -33,11 +34,11 @@ const gameBoard = (() => {
 
         function checkIfEmpty(position){ //Check if given position is Empty
                 const element = board[position];
-                if (element != null) {
-                    return true;
+                if (element != null || element != undefined) {
+                    return false;
                 }
                 else {
-                    return false;
+                    return true;
                 }
         }
 
@@ -46,6 +47,46 @@ const gameBoard = (() => {
         }
 
 
-    return {
-    }
+    return {changeBoard, checkIfEmpty, getPosition, board}
 })();
+
+// Board Operations and Methods
+const gameFlow = (() => { 
+    return {}
+})();
+
+
+function render(){ // renders positions played from the Array to the HTML page, only renders new positions played.
+    boardPositions = document.getElementsByClassName("positions");
+    for (let position of boardPositions ) {
+        let oSignImage = document.createElement("img");
+        let xSignImage = document.createElement("img");
+        oSignImage.src = "/images/osign.png";
+        xSignImage.src = "/images/xsign.png";
+        oSignImage.className = "bounce-in-fwd";
+        xSignImage.className = "bounce-in-fwd";
+        if (!position.classList.contains("played")){ // only adds new plays, not already stored plays
+            arrayStoredPosition = gameBoard.getPosition(position.id.slice(-1)); // Get Position from the Array that corresponds to the position ID of the element we are checking
+            switch (arrayStoredPosition) {
+                case "X":
+                    position.appendChild(xSignImage);  
+                    position.classList.add("played");
+                    break;
+                case "O":
+                    position.appendChild(oSignImage);  
+                    position.classList.add("played");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
+function resetRenderBoard(){ // Resets the rendered Board (for a new game)
+    boardPositions = document.getElementsByClassName("positions");
+    for (let position of boardPositions ) {
+        position.innerHTML = "";
+        position.classList.remove("played");
+    }
+}
