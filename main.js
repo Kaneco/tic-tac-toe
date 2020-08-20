1defd53971452fad1b8b50fd9f135256d06d32e2
kaneco <zegomes@gmail.com>
2020-08-20 20:51:46 +0100

    Fixed UI - added change name functionality - added end game states - added score functionality

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let player1;
+let player2;
+
 // Creates Players
 const playerFactory = (name, sign) => {
 	let score = 0;
@@ -15,13 +18,13 @@ const playerFactory = (name, sign) => {
 };
 
 // Players Created
-const player1 = playerFactory("Player1", "X");
-const player2 = playerFactory("Player2", "O");
+//const player1 = playerFactory("Player1", "X");
+//const player2 = playerFactory("Player2", "O");
 
 // Board Operations and Methods
 const gameBoard = (() => {
-	// var board = new Array(9); //Creates board to store player positions
-	let board = ["X", "X", "O", "", "X", "O", "", "", "X"];
+	let board = new Array(9); //Creates board to store player positions
+	//let board = ["X", "X", "O", "", "X", "O", "", "", "X"];
 
 	function changeBoardPosition(position, currentPlayerSign) {
 		//Update play made to the board
@@ -81,6 +84,7 @@ const gameFlow = (() => {
 		[2, 4, 6],
 	];
 
+	let gameOngoing = true; // Game is still ongoing
 	let currentPlayer = player1; // Start game always with player 1
 
 	// Toggles current player after a play
@@ -91,6 +95,12 @@ const gameFlow = (() => {
 			currentPlayer = player1;
 		}
 	};
+
+	//Reset game state to defaults after a game over
+	function resetGameState() {
+		gameOngoing = true;
+		currentPlayer = player1;
+	}
 
 	// Get Current Player
 	const getCurrentPlayer = () => currentPlayer;
@@ -111,27 +121,32 @@ const gameFlow = (() => {
 		return winningMoves.find((winningCombinaton) => {
 			return winningCombinaton.every((position) => {
 				return gameBoard.getPosition(position) == playerSign;
-			})
-		})
-	};
+			});
+		});
+	}
 
 	function gameLogic(position) {
-		// Check if play is possible
-		if (makePlay(position, currentPlayer)) {
-			// Check if the play made resulted in win
-			winningArray = checkIfWinner(currentPlayer.sign);
-			if (Array.isArray(winningArray)) {
-				return winningArray;
+		if (gameOngoing) {
+			// Check if play is possible
+			if (makePlay(position, currentPlayer)) {
+				// Check if the play made resulted in win
+				winningArray = checkIfWinner(currentPlayer.sign);
+				if (Array.isArray(winningArray)) {
+					gameOngoing = false;
+					return winningArray;
+				}
+				// If no winner, check for TIE (in case board is full)
+				else if (gameBoard.checkIfBoardFull()) {
+					gameOngoing = false;
+					return "TIE";
+				}
+				toggleCurrentPlayer(); // Toggles Current Player if Play is Sucessfull
+			} else {
+				// Play not sucessful
+				return false;
 			}
-			// If no winner, check for TIE (in case board is full)
-			else if (gameBoard.checkIfBoardFull()) {
-				return "TIE";
-			}
-			toggleCurrentPlayer(); // Toggles Current Player if Play is Sucessfull
-		} else {
-			// Play not sucessful
-			return false;
 		}
+		return false;
 	}
 
 	return {
@@ -140,6 +155,7 @@ const gameFlow = (() => {
 		makePlay,
 		checkIfWinner,
 		gameLogic,
+		resetGameState,
 	};
 })();
 
@@ -180,26 +196,69 @@ const screenDisplay = (() => {
 		for (let position of boardPositions) {
 			position.innerHTML = "";
 			position.classList.remove("played");
+			position.classList.remove("winner-position");
 		}
 	}
+
+	const triggerWinner = (winningCombination) => {};
+
+	// Restart game, resets game state and board
+	document
+		.getElementById("restart-game")
+		.addEventListener("click", function (event) {
+			resetRenderBoard();
+			gameBoard.resetBoard();
+			gameFlow.resetGameState();
+		});
+
+	// submits names
+	document
+		.getElementById("submit-btn")
+		.addEventListener("click", function (event) {
+            let p1name = document.getElementById("player1").value;
+            let p2name = document.getElementById("player2").value;
+            player1 = playerFactory(p1name, "X");
+            player2 = playerFactory(p2name, "O");
+            console.log(p1name);
+            console.log(player1);
+            document.getElementById("game-board").style.opacity = 1;
+            document.getElementById("player-names").style.display = "none";
+            document.getElementById("player-info").style.display = "flex";
+            resetRenderBoard();
+			gameBoard.resetBoard();
+            gameFlow.resetGameState();
+
+		});
 
 	document
 		.getElementById("game-board")
 		.addEventListener("click", function (event) {
 			//Game Board Event Listeners
 			if (event.target.classList.contains("positions")) {
-                let position = event.target.id.slice(-1);
-                resultOfPlay = gameFlow.gameLogic(position);
-				if (Array.isArray(resultOfPlay)){ // In case of WIN
-
-                }
-                else if (resultOfPlay == "TIE") {
-                    // In case of TIE
-                }
-
-                
-				render();
+				let position = event.target.id.slice(-1);
+				resultOfPlay = gameFlow.gameLogic(position);
+				if (Array.isArray(resultOfPlay)) {
+					// In case of WIN
+					console.log(resultOfPlay + gameFlow.getCurrentPlayer().name);
+					render();
+					// Highlights winning plays
+					document
+						.getElementById(`position-${resultOfPlay[0]}`)
+						.classList.add("winner-position");
+					document
+						.getElementById(`position-${resultOfPlay[1]}`)
+						.classList.add("winner-position");
+					document
+						.getElementById(`position-${resultOfPlay[2]}`)
+						.classList.add("winner-position");
+				} else if (resultOfPlay == "TIE") {
+					render();
+					// In case of TIE
+				} else {
+					render();
+				}
 			}
 		});
-	return { render, resetRenderBoard }
+
+	return { render, resetRenderBoard };
 })();
